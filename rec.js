2bec504666792dd7dd953d9aa4e5ffa624530149
wasifwaheed17 <wasifwaheed17@gmail.com>
2023-06-03 14:46:33 +0500

    edit button add

--- rec.js
+++ rec.js
@@ -3,6 +3,9 @@ let monBudget = 0;
 function addBudget(event) {
     event.preventDefault();
     const budgetInput = document.getElementById("mbudget");
+    if(budgetInput.value === ""){
+        alert("please fill the feild")
+    }
     monBudget = parseInt(budgetInput.value);
     budgetInput.value = ''; // Clearing the input field
     console.log(monBudget);
@@ -17,6 +20,14 @@ function addExpenses(event) {
     const payDate = document.getElementById("Pdate");
     const categ = document.getElementById("ctgr");
     const tamoun = document.getElementById("tamount");
+    if(descri.value==="" || dueDate.value ==="" || payDate.value===""||categ.value==="" ||tamoun.value===""){
+      alert("please fill the feild");
+      return;
+    }
+    // if(isNaN(tamoun)){
+    //   //console.error("error");
+
+    // }
     const expn = {
         Desc: descri.value,
         Ddate: dueDate.value,
@@ -35,35 +46,52 @@ function addExpenses(event) {
     crbudget();
     b();
     e();
+    
 }
 
 function displayExpenses() {
-    const expenseTable = document.getElementById('expenseslist');
-    const tbody = expenseTable.querySelector('tbody');
-    tbody.innerHTML = ''; // Clearing existing table data
-  
-    exp.forEach((expn) => {
-      const row = document.createElement('tr');
-      const ddateCell = document.createElement('td');
-      const tamCell = document.createElement('td');
-      const catgCell = document.createElement('td');
-      const dCell = document.createElement('td');
-      const pdateCell = document.createElement('td');
-  
-      ddateCell.textContent = expn.Ddate;
-      tamCell.textContent = expn.tamount;
-      catgCell.textContent = expn.ctgr;
-      dCell.textContent = expn.Desc;
-      pdateCell.textContent = expn.Pdate;
-  
-      row.appendChild(ddateCell);
-      row.appendChild(tamCell);
-      row.appendChild(catgCell);
-      row.appendChild(dCell);
-      row.appendChild(pdateCell);
-      tbody.appendChild(row);
-    });
-  }
+  const expenseTable = document.getElementById('expenseslist');
+  const tbody = expenseTable.querySelector('tbody');
+  tbody.innerHTML = ''; // Clearing existing table data
+
+  exp.forEach((expn, index) => {
+    const row = document.createElement('tr');
+    const ddateCell = document.createElement('td');
+    const tamCell = document.createElement('td');
+    const catgCell = document.createElement('td');
+    const dCell = document.createElement('td');
+    const pdateCell = document.createElement('td');
+    const actionCell = document.createElement('td'); // New cell for action buttons
+
+    ddateCell.textContent = expn.Ddate;
+    tamCell.textContent = expn.tamount;
+    catgCell.textContent = expn.ctgr;
+    dCell.textContent = expn.Desc;
+    pdateCell.textContent = expn.Pdate;
+
+    row.appendChild(ddateCell);
+    row.appendChild(tamCell);
+    row.appendChild(catgCell);
+    row.appendChild(dCell);
+    row.appendChild(pdateCell);
+
+    
+    const editButton = document.createElement('img');
+    editButton.src = "./resources/icons8-edit-30.png";
+    editButton.addEventListener('click', () => editExpense(index));
+
+    const deleteImage = document.createElement('img');
+    deleteImage.src = "./resources/icons8-delete-24.png";
+    deleteImage.addEventListener('click', () => deleteExpense(index));
+
+    actionCell.appendChild(editButton);
+    actionCell.appendChild(deleteImage);
+
+    row.appendChild(actionCell);
+    tbody.appendChild(row);
+  });
+}
+
   
 // function generateTableRows() {
 //     const tableBody = document.getElementById('expensesTableBody');
@@ -128,4 +156,38 @@ function e (){
   let nesw =  monBudget - ex;
   let nes = document.getElementById("expenses")
   nes.textContent =nesw ;
+}
+
+function editExpense(index) {
+  const expn = exp[index];
+
+
+  const descri = document.getElementById("Desc");
+  const dueDate = document.getElementById("Ddate");
+  const payDate = document.getElementById("Pdate");
+  const categ = document.getElementById("ctgr");
+  const tamoun = document.getElementById("tamount");
+
+  descri.value = expn.Desc;
+  dueDate.value = expn.Ddate;
+  payDate.value = expn.Pdate;
+  categ.value = expn.ctgr;
+  tamoun.value = expn.tamount;
+
+  
+  exp.splice(index, 1);
+
+  
+  displayExpenses();
+  crbudget();
+}
+
+
+function deleteExpense(index) {
+  
+  exp.splice(index, 1);
+
+  
+  displayExpenses();
+  crbudget();
 }
